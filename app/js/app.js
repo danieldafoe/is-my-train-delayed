2fd2e64171a23b6206d040964b8e280c4257caa2
Daniel Dafoe <dannydafoe@gmail.com>
2016-12-01 23:04:59 -0500

    Create app.js for client side JS; handle displaying and hiding delay info

---
+++ app/js/app.js
@@ -0,0 +1,38 @@
+// ==========================================================================
+// Application JavaScript for IMTD?
+// ==========================================================================
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+	var trainInfo = document.querySelector('.train-info');
+
+	trainInfo.addEventListener('click', function(e) {
+		handleDelayInfoClick(e);
+	});
+
+	function handleDelayInfoClick(event) {
+		event.preventDefault();
+
+		if (event.target.parentElement.parentElement.nextSibling.classList.contains('delay-info--show')) {
+			event.target.parentElement.parentElement.nextSibling.classList.remove('delay-info--show')
+		}
+		else {
+			event.target.parentElement.parentElement.nextSibling.classList.add('delay-info--show');
+		}
+	}
+	// function handleRefresh() {
+	// 	var oReq = new XMLHttpRequest();
+	// 	oReq.addEventListener("load", handleResponse);
+	// 	oReq.open("GET", "http://localhost:5000/fetch");
+	// 	oReq.send();
+	// }
+	// function handleResponse(res) {
+	// 	console.log(res);
+	// 	var data = JSON.parse(res.target.response);
+	// 	if (res.target.status === 200) {
+	// 		// OK
+			
+	// 	}
+	}
+}
