ab0ae2f4ae30eddf7dcf7fafaa18e9ef1628eaf8
Daniel Dafoe <dannydafoe@gmail.com>
2016-12-01 23:11:31 -0500

    Change URL to be a constant; improve formatting

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var pug = require('pug');
-var request = require('request');
-var cheerio = require('cheerio');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const pug = require('pug');
+const request = require('request');
+const cheerio = require('cheerio');
 
-var app = express();
+const app = express();
 
 app.set('views', './views')
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '/')));
+
+//
+// API: /
+// --------------------------------------------------------------------------
 app.get('/', function(req, res) {
 
 	// Scrape GO Transit main page
-  var url = 'http://www.gotransit.com/publicroot/en/default.aspx';
+  const url = 'http://www.gotransit.com/publicroot/en/default.aspx';
   var timestamp;
   var trainsArr = [];
 
@@ -63,33 +67,6 @@ app.trainsAreDelayed = function() {
           }
       }
   });
-}
-
-app.getTrains = function() {
-	request(url, function(error, response, html) {
-		if (!error) {
-        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-        var $ = cheerio.load(html);
-        var trains = $('.gridStatusTrain tbody tr');
-        var trainsDelayed = trains.has('.delayLink');
-        var trainsArr = [
-        	{
-        		"name": "Test",
-        		"status": false
-        	}
-        ];
-
-        // If trainsDelayed.length is 0, none are delayed
-        if (trainsDelayed.length === 0) {
-        	return false;
-        }
-        else {
-        	return true;
-        }
-    }
-	});
-
-  res.render('index', { "trains": trainsArr });
 }
 
 app.listen(process.env.PORT || 5000);
