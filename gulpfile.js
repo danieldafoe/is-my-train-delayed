3dcb219ed73020db5791271675b27cc7228d85c9
Daniel Dafoe <dannydafoe@gmail.com>
2016-12-22 09:13:46 -0500

    Change hard tabs to soft tabs

--- gulpfile.js
+++ gulpfile.js
@@ -6,51 +6,51 @@ const webpack = require('webpack-stream');
 const uglify = require('gulp-uglify');
 
 var config = {
-	"pug": {
-		"src": "./app/pug/*.pug",
-		"dest": "./"
-	},
-	"sass": {
-		"src": "./app/sass/*.scss",
-		"dest": "./css/"
-	},
-	"js": {
-		"src": "./app/js/*.js",
-		"dest": "./js/"
-	}
+  "pug": {
+    "src": "./app/pug/*.pug",
+    "dest": "./"
+  },
+  "sass": {
+    "src": "./app/sass/*.scss",
+    "dest": "./css/"
+  },
+  "js": {
+    "src": "./app/js/*.js",
+    "dest": "./js/"
+  }
 };
 
 gulp.task('pug', () => {
-	return gulp.src(config.pug.src)
-	.pipe(pug())
-	.pipe(gulp.dest(config.pug.dest))
+  return gulp.src(config.pug.src)
+  .pipe(pug())
+  .pipe(gulp.dest(config.pug.dest))
 });
 
 gulp.task('sass', () => {
-	return gulp.src(config.sass.src)
-	.pipe(sass())
-	.pipe(gulp.dest(config.sass.dest))
+  return gulp.src(config.sass.src)
+  .pipe(sass())
+  .pipe(gulp.dest(config.sass.dest))
 });
 
 gulp.task('js', function() {
-	return gulp.src(config.js.src)
-	.pipe(webpack({
-		watch: true,
-		module: {
+  return gulp.src(config.js.src)
+  .pipe(webpack({
+    watch: true,
+    module: {
       loaders: [
         { test: /\.jsx?/, loader: 'babel' },
       ],
     },
-	}))
-	.pipe(rename(function(path) {
-		path.basename = 'app'
-	}))
-	.pipe(gulp.dest('./js/'));
+  }))
+  .pipe(rename(function(path) {
+    path.basename = 'app'
+  }))
+  .pipe(gulp.dest('./js/'));
 });
 
 gulp.task('watch', () => {
-	gulp.watch(config.sass.src, ['sass']);
-	gulp.watch(config.js.src, ['js']);
+  gulp.watch(config.sass.src, ['sass']);
+  gulp.watch(config.js.src, ['js']);
 });
 
 gulp.task('default', ['watch']);
