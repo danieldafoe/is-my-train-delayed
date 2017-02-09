c08d2d70de0a7a755c5d6f1f0b4d3f8ccbacd61e
Daniel Dafoe <dannydafoe@gmail.com>
2017-02-08 20:21:32 -0500

    Add minifying of CSS and writing sourcemaps to Sass gulp task ✨

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+const clean = require('gulp-clean-css');
 const gulp = require('gulp');
 const pump = require('pump');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sassLint = require('gulp-sass-lint');
+const sourcemaps = require('gulp-sourcemaps');
 const wp = require('webpack');
 const webpack = require('webpack-stream');
 const uglify = require('gulp-uglify');
@@ -38,6 +40,12 @@ gulp.task('sass', () => {
   .pipe(sassLint.format())
   .pipe(sassLint.failOnError())
   .pipe(sass())
+  .pipe(sourcemaps.init())
+  .pipe(clean({ compatibility: 'ie11' }, (details) => {
+    console.log(details.name + ': ' + details.stats.originalSize);
+    console.log(details.name + ': ' + details.stats.minifiedSize);
+  }))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest(config.sass.dest))
 });
 
